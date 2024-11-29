e8aae4a629e98a430f7e7e272ad21178d1aef3db
Oleg Skvortsov <highsoft.soi@gmail.com>
2024-11-29 13:57:32 -0800

    Added possibility to connect to MQTT server and get command from it.

--- alarm_receiver.ino
+++ alarm_receiver.ino
@@ -1,11 +1,18 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 #define wifi_ssid "Soi13"
 #define wifi_password ""
 
 #define mqtt_server "10.0.0.4"
+#define mqtt_port 1883
 #define mqtt_user "mqtt_user"
 #define mqtt_password ""
+
+#define alarm_state_topic "homeassistant/sensor/fire_alarm"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 const int ledPin = 10;
 const int buzzerPin = 8;
@@ -36,6 +43,44 @@ void reconnectWiFi() {
   Serial.println("\nReconnected to Wi-Fi!");
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received: ");
+  Serial.println(topic);
+
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.print("Payload: ");
+  Serial.println(message);
+
+  // Control buzzer based on the command
+  if (message == "ON") {
+    digitalWrite(buzzerPin, HIGH);
+    Serial.println("Buzzer ON");
+  } else if (message == "OFF") {
+    digitalWrite(buzzerPin, LOW);
+    Serial.println("Buzzer OFF");
+  } else {
+    Serial.println("Unknown command");
+  }
+}
+
+void reconnect_MQTT_broker() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("XIAO_ESP32C3_Client", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe(alarm_state_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -58,6 +103,9 @@ void setup() {
   Serial.println("\nWi-Fi connected!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
@@ -68,6 +116,11 @@ void loop() {
   } else {
     digitalWrite(ledPin, LOW);
   }
+
+  if (!client.connected()) {
+    reconnect_MQTT_broker();
+  }
+  client.loop();
 
   //Test code block for simulating of temporary WiFi connection.
   /*if (millis() - startTime > disconnectAfter) {
